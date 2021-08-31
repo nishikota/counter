261410a0ccd2179e0b81413318984af4a729c852
西航太 <>
2021-08-31 13:13:28 +0900

    カウント機能　実装

---
+++ reduxcounter/src/components/counter.js
@@ -0,0 +1,43 @@
+import {useState} from "react";
+import {useSelector, useDispatch} from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  incrementAsync,
+  incrementIfOdd,
+  selectCount,
+} from "./counterSlice";
+
+export const Counter = () => {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState("2");
+
+  const incrementValue = Number(incrementAmount) || 0;
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => dispatch(decrement())}>-</button>
+        <span>{count}</span>
+        <button onClick={() => dispatch(increment())}>+</button>
+      </div>
+      <div>
+        <input
+          value={incrementAmount}
+          onChange={(e) => setIncrementAmount(e.target.value)}
+        />
+        <button onClick={() => dispatch(incrementByAmount(incrementValue))}>
+          Add Amount
+        </button>
+        <button onClick={() => dispatch(incrementAsync(incrementValue))}>
+          Add Async
+        </button>
+        <button onClick={() => dispatch(incrementIfOdd(incrementValue))}>
+          Add If Odd
+        </button>
+      </div>
+    </div>
+  );
+};
